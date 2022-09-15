af442abbd2d224de2dc295f639e861299449ab2c
tiagoamp <tiagoamp@gmail.com>
2022-09-15 20:45:01 -0300

    Arquivos criados na aula 4

---
+++ src/Gato.java
@@ -0,0 +1,19 @@
+public class Gato {
+
+    String nome;
+    Integer idade;
+
+    public String miar() {
+        return "miauuuuuu";
+    }
+
+    public String andar() {
+        return "estou andando";
+    }
+
+    public String toString() {
+        return "O gato é: " + nome + ", " + idade;
+    }
+
+}
+
